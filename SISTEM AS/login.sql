1d1385953c3ac40c04201405ebcd520130fd131f
martinsteigen44 <45411810+martinsteigen44@users.noreply.github.com>
2018-12-01 01:48:38 -0600

    CAMBIO PASSWORD A CONTRASEÑA EN RUTA DE SISTEMAS EN LOGIN.SQL

--- SISTEM AS/login.sql
+++ SISTEM AS/login.sql
@@ -58,7 +58,7 @@ CREATE TABLE IF NOT EXISTS `usuarios` (
 -- Volcado de datos para la tabla `usuarios`
 --
 
-INSERT INTO `usuarios` (`id`, `nombre`, `apellidos`, `correo`, `password`, `cargo_id`) VALUES
+INSERT INTO `usuarios` (`id`, `nombre`, `apellidos`, `correo`, `contraseña`, `cargo_id`) VALUES
 (11, 'Alicia', 'garcia', 'alix@live.com.mx', '*0', 2),
 (13, 'Admin', 'mendoza', 'admin@live.com.mx', '*0', 1),
 (17, 'Jose alfredo', 'roman cruz', 'pery@gmail.com', 'qwerty', 2),
