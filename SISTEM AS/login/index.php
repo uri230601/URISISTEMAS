c6c825df8dba7428dbeee95239275372daf867fb
urielyomero <yukitero.0.1@gmail.com>
2018-12-01 00:33:50 -0600

    cambio de usuario1.0.1

--- SISTEM AS/login/index.php
+++ SISTEM AS/login/index.php
@@ -33,7 +33,7 @@ if(isset($_SESSION['session']))
 					<!--ejemplo <label for="usuario">Usuario:</label>-->
 					<!--required es nuevo en html5, si el campo está vacío te avisa, pero cuidado, no valida la información-->
 					<p><label >Nombre de Usuario:</label></p>
-						<input name="usuario" type="text" id="usuario" placeholder="Ingresa Usuario" autofocus="" required=""></p>
+						<input name="Nombre" type="text" id="usuario" placeholder="Ingresa location" autofocus="" required=""></p>
 					
 					<p><label>Clave:</label></p>
 						<input name="contrasenia" type="password" id="contrasenia" placeholder="Ingresa Password" required=""></p>
