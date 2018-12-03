d5384400d17653edbee16311c3b2756a6c378dc8
uri230601 <45440650+uri230601@users.noreply.github.com>
2018-12-03 11:46:22 -0600

    cambio de registrar a inscribete

--- SISTEM AS/login/index.php
+++ SISTEM AS/login/index.php
@@ -17,7 +17,7 @@ if(isset($_SESSION['session']))
 <body>
 	
 	<div id="sistemas de información">
-          	<a href="./php/Ingresa.php"</a>Registrar</a>
+          	<a href="./php/Ingresa.php"</a>inscribete</a>
     </div> <!-- fin opcion-->
     
 	<div id="usuario">
