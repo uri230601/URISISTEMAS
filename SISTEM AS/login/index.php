8b2596135d6bfcf045dd6568a52b0b697a3c6bda
COBAOcoco <COBAOcoco@gmail.com>
2018-12-04 12:22:13 -0600

    nombre para usuario

--- SISTEM AS/login/index.php
+++ SISTEM AS/login/index.php
@@ -17,7 +17,7 @@ if(isset($_SESSION['session']))
 <body>
 	
 	<div id="sistemas de información">
-          	<a href="./php/Ingresa.php"</a>ANOTATE</a>
+          	<a href="./php/Ingresa.php"</a>registrate</a>
     </div> <!-- fin opcion-->
     
 	<div id="usuario">
