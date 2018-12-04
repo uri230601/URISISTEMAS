65572b207dd77d2a725c7a2e0465fd06cdd451f6
COBAOcoco <COBAOcoco@gmail.com>
2018-12-04 12:02:48 -0600

    nueva

--- SISTEM AS/login/index.php
+++ SISTEM AS/login/index.php
@@ -42,7 +42,7 @@ if(isset($_SESSION['session']))
 				</form>
 			</div><!--fin cuerpo-->
 	
-			<div id="pie">hola soy la pajarita</div>
+			<div id="pie">uriel gay</div>
 		</div><!-- fin contenedor -->
 
 	</div><!--fin envoltura-->
